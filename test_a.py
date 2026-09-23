25378a75b1010f9c98c969e8d8f9b0ddcba06d85
ltyity <ltyity@qq.com>
2026-09-23 18:35:21 +0800

    1.0.3 自动化测试

---
+++ test_a.py
@@ -0,0 +1,11 @@
+def test_C001001():
+    print('\n用例c001001')
+    assert 1 == 1
+
+def test_C001002():
+    print('\n用例c001002')
+    assert 2 == 2
+
+def test_C001003():
+    print('\n用例C001003')
+    assert 3 == 2
