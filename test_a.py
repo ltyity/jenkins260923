f9fd0edb89e9181d8d50be886cb060202e50d115
ltyity <ltyity@qq.com>
2026-09-23 18:56:42 +0800

    1.0.4 修改测试用例

--- test_a.py
+++ test_a.py
@@ -4,8 +4,12 @@ def test_C001001():
 
 def test_C001002():
     print('\n用例c001002')
-    assert 2 == 2
+    assert 2 == 1
 
 def test_C001003():
     print('\n用例C001003')
     assert 3 == 2
+
+def test_C001004():
+    print('\n用例C001003')
+    assert 4 == 4
